839210851de0ff96cf7575b72cf4b4c76d1008ab
jprice628 <jprice628@hotmail.com>
2024-07-27 18:32:55 -0400

    Added FizzBuzz Solution (#1)
    * Added FizzBuzz solution files
    
    * Added debug command line argument

---
+++ FizzBuzz/fb.bat
@@ -0,0 +1,12 @@
+@echo off
+
+REM This could be simplified to a single dotnet run call, but it is slower.
+
+set file=".\bin\Debug\net8.0\FizzBuzz.exe"
+
+if not exist %file% (
+	echo Building executable...
+	dotnet build
+)
+
+%file% %*
