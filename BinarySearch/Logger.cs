39b653d00ddf3aab67214a835eb12879c6d17989
jprice628 <jprice628@hotmail.com>
2024-07-28 01:48:39 -0400

    Added BinarySearch solution (#2)
    * Added BinarySearch solution
    
    * Implemented logging

---
+++ BinarySearch/Logger.cs
@@ -0,0 +1,12 @@
+﻿using LanguageExt.TypeClasses;
+
+namespace BinarySearch;
+
+internal readonly record struct Logger : Monoid<string>
+{
+    public string Append(string x, string y) =>
+        x + y;
+
+    public string Empty() =>
+        string.Empty;
+}
