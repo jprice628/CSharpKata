8670ed8d27475bd152ec7408eb56069831f5a1b9
jprice628 <jprice628@hotmail.com>
2024-07-28 22:46:20 -0400

    Added Act and Assert comments to ThingIdTests.cs

--- Things/DomainTests/ThingIdTests.cs
+++ Things/DomainTests/ThingIdTests.cs
@@ -9,9 +9,11 @@ public class ThingIdTests
     [TestMethod]
     public void ThingId_New_ReturnsThingIdFromGuid()
     {
+        // Act
         var guid = new Guid("aaa7f552-7607-4cf9-bd01-d9fa7f470be2");
         var result = ThingId.New(guid);
 
+        // Assert
         result.IsSucc.Should().BeTrue();
         result.Value().Value.Should().Be(guid);
     }
@@ -19,9 +21,11 @@ public class ThingIdTests
     [TestMethod]
     public void ThingId_New_ReturnsThingIdFromString()
     {
+        // Act
         var guidAsString = "aaa7f552-7607-4cf9-bd01-d9fa7f470be2";
         var result = ThingId.New(guidAsString);
 
+        // Assert
         result.IsSucc.Should().BeTrue();
         result.Value().Value.Should().Be(new Guid(guidAsString));
     }
@@ -29,8 +33,10 @@ public class ThingIdTests
     [TestMethod]
     public void ThingId_New_ReturnsEmptyGuidError()
     {
+        // Act
         var result = ThingId.New(Guid.Empty);
 
+        // Assert
         result.IsSucc.Should().BeFalse();
         result.Error().Message.Should().Be("ThingId: Value cannot be an empty GUID.");
     }
@@ -38,9 +44,11 @@ public class ThingIdTests
     [TestMethod]
     public void ThingId_New_ReturnsUnableToParseError()
     {
+        // Act
         var guidAsString = "xyz123";
         var result = ThingId.New(guidAsString);
 
+        // Assert
         result.IsSucc.Should().BeFalse();
         result.Error().Message.Should().Be("ThingId: Unable to parse GUID.");
     }
