44fa60db73df1dc8ad4165201a409923efd1ab5b
jprice628 <jprice628@hotmail.com>
2024-07-28 23:03:23 -0400

    Added Size

---
+++ Things/Domain/Size.cs
@@ -0,0 +1,40 @@
+﻿using LanguageExt;
+using LanguageExt.Common;
+
+namespace Domain;
+
+/// <summary>
+/// Provides an expressive way to describe the size of a thing
+/// </summary>
+/// <remarks>
+/// Thing sizes are values, so they benefit from being represented using records 
+/// instead of classes. A record class is being used instead of a record struct
+/// because the value has a universal constraint: it has to be between 1 and 10.
+/// </remarks>
+public sealed record Size
+{
+    /// <summary>
+    /// The size value, a number between 1 and 10 (inclusive)
+    /// </summary>
+    public int Value { get; private init; }
+
+    /// <summary>
+    /// Constructs a new size value
+    /// </summary>
+    /// <param name="value">The integer value of the size</param>
+    /// <remarks>
+    /// This constructor expects the factory method below to validate the value
+    /// </remarks>
+    private Size(int value) =>
+        Value = value;
+
+    /// <summary>
+    /// Constructs a new size value
+    /// </summary>
+    /// <param name="value">An integer from 1 to 10 (inclusive)</param>
+    /// <returns>A size value or an error</returns>
+    public static Fin<Size> New(int value) =>
+        value < 1 ? Error.New("Size: value cannot be less than 1.")
+        : value > 10 ? Error.New("Size: value cannot be greater than 10.")
+        : new Size(value);
+}
---
+++ Things/DomainTests/SizeTests.cs
@@ -0,0 +1,41 @@
+﻿using Domain;
+using FluentAssertions;
+
+namespace DomainTests;
+
+[TestClass]
+public class SizeTests
+{
+    [TestMethod]
+    public void Size_New_ReturnsASizeValue()
+    {
+        // Act
+        var result = Size.New(5);
+
+        // Assert
+        result.IsSucc.Should().BeTrue();
+        result.Value().Value.Should().Be(5);
+    }
+
+    [TestMethod]
+    public void Size_New_ReturnsSizeCannotBeLessThan1Error()
+    {
+        // Act
+        var result = Size.New(0);
+
+        // Assert
+        result.IsSucc.Should().BeFalse();
+        result.Error().Message.Should().Be("Size: value cannot be less than 1.");
+    }
+
+    [TestMethod]
+    public void Size_New_ReturnsSizeCannotGreaterThan10Error()
+    {
+        // Act
+        var result = Size.New(11);
+
+        // Assert
+        result.IsSucc.Should().BeFalse();
+        result.Error().Message.Should().Be("Size: value cannot be greater than 10.");
+    }
+}
