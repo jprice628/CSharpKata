cb772501bb479de0a24910c257baedb112219552
jprice628 <jprice628@hotmail.com>
2024-07-28 23:25:56 -0400

    Added Timestamp

---
+++ Things/Domain/Timestamp.cs
@@ -0,0 +1,37 @@
+﻿using LanguageExt;
+using LanguageExt.Common;
+
+namespace Domain;
+
+/// <summary>
+/// Provides an expressive way to represent the time at which an event occurred
+/// </summary>
+/// <remarks>
+/// Timestamps are value types, so they benefit from being represented using 
+/// records. They do not have an universal constraints, so a record struct is 
+/// used. 
+/// </remarks>
+public readonly record struct Timestamp(DateTimeOffset Value)
+{
+    /// <summary>
+    /// Provides a timestamp for the current time in UTC
+    /// </summary>
+    public static Timestamp UtcNow => 
+        new Timestamp(DateTimeOffset.UtcNow);
+
+    /// <summary>
+    /// Constructs a timestamp from a string value
+    /// </summary>
+    /// <param name="valueAsString">A string representing a DateTimeOffset value</param>
+    /// <returns>A timestamp or an error</returns>
+    public static Fin<Timestamp> New(string valueAsString) =>
+        !DateTimeOffset.TryParse(valueAsString, out var value) ? Error.New("Timestamp: Unable to parse string.")
+        : new Timestamp(value);
+
+    /// <summary>
+    /// Converts the timestamp to universal time
+    /// </summary>
+    /// <returns>A timestamp</returns>
+    public Timestamp ToUniversalTime() =>
+        this with { Value = Value.ToUniversalTime() };
+}
---
+++ Things/DomainTests/TimestampTests.cs
@@ -0,0 +1,56 @@
+﻿using Domain;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DomainTests;
+
+[TestClass]
+public class TimestampTests
+{
+    [TestMethod]
+    public void Timestamp_UtcNow_ReturnsUtcTimestamp()
+    {
+        // Act
+        var ts = Timestamp.UtcNow;
+
+        // Assert
+        ts.Value.Offset.Should().Be(TimeSpan.Zero);
+        ts.Value.Should().BeCloseTo(DateTimeOffset.UtcNow, TimeSpan.FromSeconds(2));
+    }
+
+    [TestMethod]
+    public void Timestamp_New_ReturnsTimestampFromString()
+    {
+        // Act
+        var str = "7/29/2024 3:17:48 AM +00:00";
+        var result = Timestamp.New(str);
+
+        // Assert
+        result.IsSucc.Should().BeTrue();
+        result.Value().Value.Should().Be(DateTimeOffset.Parse(str));
+    }
+
+    [TestMethod]
+    public void Timestamp_New_ReturnsUnableToParseError()
+    {
+        // Act
+        var str = "xyz";
+        var result = Timestamp.New(str);
+
+        // Assert
+        result.IsSucc.Should().BeFalse();
+        result.Error().Message.Should().Be("Timestamp: Unable to parse string.");
+    }
+
+    [TestMethod]
+    public void Timestamp_ToUniversalTime_ReturnsUtcTimestamp()
+    {
+        // Act
+        var dt = DateTimeOffset.Parse("7/29/2024 12:00:00 PM -04:00");
+        var ts = new Timestamp(dt).ToUniversalTime();
+
+        // Assert
+        ts.Value.Offset.Should().Be(TimeSpan.Zero);
+        ts.Value.Should().Be(DateTimeOffset.Parse("7/29/2024 04:00:00 PM +00:00"));
+    }
+}
