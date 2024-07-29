b1cbbf6b48902c597f4b03a7e23c44293a54e224
jprice628 <jprice628@hotmail.com>
2024-07-29 17:06:52 -0400

    Added events

---
+++ Things/Domain/Events.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain;
+
+public abstract record ThingEvent(Timestamp Timestamp);
+
+public sealed record CreatedEvent(Timestamp Timestamp, ThingId ThingId, Shape Shape, Size Size)
+    : ThingEvent(Timestamp);
+
+public sealed record ReshapedEvent(Timestamp Timestamp, Shape NewShape)
+    : ThingEvent(Timestamp);
+
+public sealed record ResizedEvent(Timestamp Timestamp, Size NewSize)
+    : ThingEvent(Timestamp);
