58ac85fe6fa385dd6626dab579c1bae5152222fa
jprice628 <jprice628@hotmail.com>
2024-08-03 10:18:02 -0400

    Added persistence prelude

---
+++ Things/Persistence/Prelude/IO.cs
@@ -0,0 +1,19 @@
+﻿namespace Things.Persistence;
+
+internal static class IO
+{
+    internal static Try<Unit> WriteToFile(string path, IEnumerable<string> lines) =>
+        Try(() => { File.WriteAllLines(path, lines); return unit; });
+
+    internal static TryAsync<Unit> WriteToFileAsync(string path, IEnumerable<string> lines, CancellationToken cancellationToken = default) =>
+        TryAsync(async () => { await File.WriteAllLinesAsync(path, lines, cancellationToken); return unit; });
+
+    internal static Try<string[]> ReadFromFile(string path) =>
+        Try(() => File.ReadAllLines(path));
+
+    internal static TryAsync<string[]> ReadFromFile(string path, CancellationToken cancellationToken = default) =>
+        TryAsync(() => File.ReadAllLinesAsync(path, cancellationToken));
+
+    internal static Try<FileInfo[]> Find(DirectoryInfo storageDirectory, string searchPattern) =>
+        Try(() => storageDirectory.GetFiles(searchPattern));
+}
---
+++ Things/Persistence/Prelude/JObjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Things.Persistence;
+
+internal static class JObjectExtensions
+{
+    internal static Fin<TValue> GetRequiredValue<TValue>(this JObject obj, string key) =>
+        !obj.TryGetValue(key, out var token) ? Error.New("JObject.GetRequiredValue: key not found.")
+        : token is null ? Error.New("JObject.GetRequiredValue: token is null")
+        : Try(() => token.Value<TValue>()).Try().Match<Fin<TValue>>(
+            Succ: value => value is null ? Error.New("JObject.GetRequiredValue: required value is null") : value,
+            Fail: ex => Error.New(ex));
+}
