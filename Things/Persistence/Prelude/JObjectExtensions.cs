0da9945b415c10d27afdf7964a81738fcf005d08
jprice628 <jprice628@hotmail.com>
2024-08-05 07:29:16 -0400

    Refactored JObjectExtensions

--- Things/Persistence/Prelude/JObjectExtensions.cs
+++ Things/Persistence/Prelude/JObjectExtensions.cs
@@ -5,17 +5,21 @@ namespace Things.Persistence;
 internal static class JObjectExtensions
 {
     internal static Either<Error, TValue> GetRequiredValue<TValue>(this JObject obj, string key) =>
+        from token in GetToken(obj, key)
+        from nullable in TryGetValue<TValue>(token)
+        from value in EnsureNonNullValue(nullable)
+        select value;
+
+    private static Either<Error, JToken> GetToken(JObject obj, string key) =>
         !obj.TryGetValue(key, out var token) ? Error.New($"JObject does not have the specified key: {key}.")
         : token is null ? Error.New("The token returned by the JObject is null.")
-        : from nullable in GetValue<TValue>(token)
-          from value in EnsureNonNull(nullable)
-          select value;
+        : token;
 
-    private static Either<Error, TValue?> GetValue<TValue>(JToken token) =>
+    private static Either<Error, TValue?> TryGetValue<TValue>(JToken token) =>
         Try(() => token.Value<TValue>())
         .ToEither(ex => Error.New("An error occurred while getting the value of a JToken: " + ex.Message));
 
-    private static Either<Error, TValue> EnsureNonNull<TValue>(TValue? value) =>
+    private static Either<Error, TValue> EnsureNonNullValue<TValue>(TValue? value) =>
         value is null ? Error.New("The required value obtained from a JToken is null.")
         : value;
 }
