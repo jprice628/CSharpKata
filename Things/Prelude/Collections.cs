8983f0ee8d781c6f34e49e6120f298a1acc257b1
jprice628 <jprice628@hotmail.com>
2024-08-06 23:03:58 -0400

    Added WhileSuccess function

--- Things/Prelude/Collections.cs
+++ Things/Prelude/Collections.cs
@@ -22,4 +22,27 @@ public static partial class Prelude
     public static Either<Error, T> FirstItem<T>(this IEnumerable<T> collection) =>
         from _ in ErrorIfEmpty(collection, "Cannot get the head of an empty collection.")
         select collection.First();
+
+
+    /// <summary>
+    /// Invokes a unit function for each item in a collection until the entire collection has been processed or an error occurs.
+    /// </summary>
+    /// <typeparam name="TSource">The type of the source collection</typeparam>
+    /// <param name="source">The source collection</param>
+    /// <param name="func">The function to invoke for each item in the source collection</param>
+    /// <returns>A unit or an error</returns>
+    public static Either<Error, Unit> WhileSuccess<TSource>(this IEnumerable<TSource> source, Func<TSource, Either<Error, Unit>> func)
+    {
+        foreach (var item in source)
+        {
+            var result = func(item);
+
+            if (result.Case is Error err)
+            {
+                return err;
+            }
+        }
+
+        return unit;
+    }
 }
--- Things/PreludeTests/CollectionsTests.cs
+++ Things/PreludeTests/CollectionsTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Things;
 
 namespace PreludeTests;
 
@@ -51,5 +52,43 @@ public class CollectionsTests
         // Assert
         result.IsRight.Should().BeTrue();
         result.Value().Should().Be(1);
+    }
+
+    [TestMethod]
+    public void Collections_WhileSuccess_IteratesOverEntireCollection()
+    {
+        // Arrange
+        var list = new List<int>();
+        Either<Error, Unit> AddToList(int n) { list.Add(n); return unit; }
+
+        // Act
+        var collection = new[] { 1, 2, 3 };
+        var result = collection.WhileSuccess(AddToList);
+
+        // Assert
+        result.IsRight.Should().BeTrue();
+        list.Should().BeEquivalentTo(collection);
+    }
+
+    [TestMethod]
+    public void Collections_WhileSuccess_FailsFast()
+    {
+        // Arrange
+        var list = new List<int>();
+        
+        Either<Error, Unit> AddOddsFailEvens(int n)
+        {
+            if (n % 2== 0) { return Error.New("Even!"); }
+            else { list.Add(n); return unit; }
+        }
+
+        // Act
+        var collection = new[] { 1, 2, 3 };
+        var result = collection.WhileSuccess(AddOddsFailEvens);
+
+        // Assert
+        result.IsRight.Should().BeFalse();
+        result.Error().Message.Should().Be("Even!");
+        list.Should().BeEquivalentTo(collection.Take(1));
     }
 }
